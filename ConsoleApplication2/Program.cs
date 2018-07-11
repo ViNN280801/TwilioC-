80e11c71cffb32603bd02eb5e57d54e2085a45fc
СемыкинВ <@406_14>
2018-07-11 20:48:43 +0600

    Add project files.

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Twilio;
+using Twilio.Base;
+using Twilio.Clients;
+using Twilio.Converters;
+using Twilio.Exceptions;
+using Twilio.Types;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string TwilioSID = "";
+            const string TwilioTOKEN = "";
+
+            TwilioClient.Init(TwilioSID, TwilioTOKEN);
+
+           var message =  MessageResource.Create(
+                to: new PhoneNumber(""),
+                from: new PhoneNumber(""),
+                body: "");
+
+            Console.WriteLine(message.Sid);
+        }
+    }
+}
